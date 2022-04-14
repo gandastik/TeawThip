bfaf997ea267d1c74541b53e8ee8e29d1c7defd3
gandastik <gandastik@gmail.com>
2022-04-14 19:30:43 +0700

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./app.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="">
+      <Navbar />
       <div className="top"></div>
     </div>
   );

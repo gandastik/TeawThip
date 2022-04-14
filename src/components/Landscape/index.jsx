b9c0c0768b1c55ed6d2392a7e74ebc2fbccdf713
gandastik <gandastik@gmail.com>
2022-04-14 19:42:44 +0700

    add landscape layout

---
+++ src/components/Landscape/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Landscape = () => {
+  return (
+    <div className="h-screen">
+      <h1 className="text-5xl text-white font-medium text-center pt-7">
+        Landscape
+      </h1>
+    </div>
+  );
+};
+
+export default Landscape;

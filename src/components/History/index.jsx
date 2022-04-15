f6a7a06f2e1e4308d07b1e8d0c0fef75510c6e96
gandastik <gandastik@gmail.com>
2022-04-15 11:29:44 +0700

    change font and add lorem in history text parts

--- src/components/History/index.jsx
+++ src/components/History/index.jsx
@@ -4,14 +4,40 @@ import "./style.css";
 const History = () => {
   return (
     <div className="bg-img w-full flex justify-start items-center">
-      <div className=" w-full h-[360px] max-w-screen-2xl mx-auto">
-        <h2 className=" text-white text-4xl font-normal leading-relaxed tracking-wide">
-          History of
-        </h2>
-        <h1 className="text-white text-6xl font-semibold tracking-wide">
-          Chiangmai
-        </h1>
-        <hr className="mt-3 border-2 bg-white  w-[400px] " />
+      <div className=" w-full h-[600px] max-w-screen-2xl mx-auto flex justify-between">
+        <div className="mt-24">
+          <h2 className=" text-white text-4xl font-normal leading-relaxed tracking-wide">
+            History of
+          </h2>
+          <h1 className="text-white text-6xl font-semibold tracking-wide">
+            Chiangmai
+          </h1>
+          <hr className="mt-3 border-2 bg-white  w-[400px] " />
+          <h3 className="text-white text-2xl font-normal tracking-wider mt-3">
+            ประวัติจังหวัดเชียงใหม่
+          </h3>
+        </div>
+
+        <div className="self-center max-w-2xl text-white font-normal text-lg">
+          <p className="indent-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            quisquam consequatur nemo possimus sequi et sunt iste tempora vero
+            quam fugit deserunt veritatis obcaecati nostrum quo facilis, maxime
+            aut iure provident excepturi eligendi quae quibusdam enim
+            exercitationem. Sequi distinctio id eius cumque perferendis?
+            Cupiditate possimus iure odit facere autem! Harum error et magni,
+            autem modi enim, debitis accusamus expedita tempora aut nostrum?
+            Iste quam odio non at sint, repellendus nobis ullam sequi error esse
+            consectetur, nemo optio adipisci minima eligendi totam. Sapiente
+            molestiae nostrum doloremque! Facilis explicabo quidem dolore at
+            quasi quisquam veritatis fugit, iste porro ipsa incidunt ab ad iure
+            placeat nesciunt mollitia quas magni a perspiciatis? Deserunt quidem
+            cupiditate modi ipsum exercitationem reiciendis aliquid delectus,
+            possimus beatae ex odio, et laudantium. Laudantium molestias
+            consequatur eos dicta veniam, eum magnam tempora alias architecto
+            quo dolor! Impedit inventore odio vitae.
+          </p>
+        </div>
       </div>
     </div>
   );
